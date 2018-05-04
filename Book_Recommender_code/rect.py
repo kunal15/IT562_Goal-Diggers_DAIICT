ef5b1e0cd49ec20bfa0721f5c565f88868aad67c
karanraj22 <sainikaran2222@gmail.com>
2018-05-05 02:01:08 +0530

    Update rect.py

--- Book_Recommender_code/rect.py
+++ Book_Recommender_code/rect.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'recwindow.ui'
-#
-# Created by: PyQt5 UI code generator 5.6
-#
-# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from greedy_ucb import *
